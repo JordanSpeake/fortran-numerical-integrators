0df3a5b16c74846e93e07f4a221fba6fb5c297cd
J Speake <j.speake@york.ac.uk>
2026-01-19 10:35:59 +0000

    Document the run bash script

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
+# If there's an error, quit the build and run process
 set -e
+
+# Make sure we're in the build/ directory
 mkdir -p build && cd build
+
+# Run cmake
 cmake ..
 cmake --build .
+
+# Run the program
 ./program
+
+# Run the analysis file inside the uv environment
 uv run "../analysis/analysis.py"
 
